72a67da908acb5070f508f43a69a8589f9846456
shichao <461919931@qq.com>
2018-06-26 17:42:46 +0800

    add bubbleSort method

---
+++ AlgorithmDemo/BubbleSort.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmDemo
+{
+    class BubbleSort
+    {
+        public void bubbleSort(int[] arry)
+        {
+            int x = 0; //用于交换的容器
+            for (int i = 0; i < arry.Length - 1; i++)
+            {
+                for (int j = 0; j < arry.Length - 1 - i;j++)
+                {
+                    if (arry[j] > arry[j + 1])
+                    {
+                        x = arry[j];
+                        arry[j] = arry[j + 1];
+                        arry[j + 1] = x;
+                    }
+                }
+            }
+        }
+    }
+}
--- AlgorithmDemo/Program.cs
+++ AlgorithmDemo/Program.cs
@@ -6,7 +6,22 @@ namespace AlgorithmDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arry = { 4, 3, 5, 1, 6, 2 };
+            Console.WriteLine("冒泡排序前的数组：");
+            foreach (int n in arry)
+            {
+                Console.Write($"{n} ");
+            }
+            Console.WriteLine();
+
+            BubbleSort bs = new BubbleSort();
+            bs.bubbleSort(arry);
+            Console.WriteLine("冒泡排序后的数组：");
+            foreach (int n in arry)
+            {
+                Console.Write($"{n} ");
+            }
+
             Console.ReadKey();
         }
     }
