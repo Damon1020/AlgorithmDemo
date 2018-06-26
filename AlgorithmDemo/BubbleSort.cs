3ce7eb168a7226fab454f7b5060ad593c38701a0
shichao <461919931@qq.com>
2018-06-26 18:14:46 +0800

    add commit

--- AlgorithmDemo/BubbleSort.cs
+++ AlgorithmDemo/BubbleSort.cs
@@ -9,8 +9,10 @@ namespace AlgorithmDemo
         public void bubbleSort(int[] arry)
         {
             int x = 0; //用于交换的容器
+            // i表示需要比较的轮数
             for (int i = 0; i < arry.Length - 1; i++)
             {
+                //(arry.Length - 1 - i)表示该轮比较后，得出的最大数所存放的位置
                 for (int j = 0; j < arry.Length - 1 - i;j++)
                 {
                     if (arry[j] > arry[j + 1])
